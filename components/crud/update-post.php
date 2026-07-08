5cd52ce65fe2e62fd3f7172307590153ef343a4f
campsaurele <campsaurele@gmail.com>
2026-07-08 14:48:14 +0200

    Add: update (work in progress)

---
+++ components/crud/update-post.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once(__DIR__.'/../../config/bdd_connect.php');
+require_once(__DIR__.'/../function/transformPlaceholder.php');
+require_once(__DIR__.'/../function/transformSetdata.php');
+require_once(__DIR__.'/../function/cleanPost.php');
+require_once(__DIR__.'/../function/showPost.php');
+require_once(__DIR__.'/../button.php');
+require_once(__DIR__.'/../function/validatePost.php');
+
+
+
+if (isset($_GET['t']) && isset($_GET['id'])) {
+    switch ($_GET['t']) {
+        case "articles":
+            $table = "articles_presse";
+            $title = "Articles";
+            $required = ["titre","auteur","contenu","date_publication","match_id"];
+            $id = $_GET['id'];
+            break;
+        case "results":
+            $table = "resultats_sportifs";
+            $title = "Résultat";
+            $required = ["equipe1","equipe2","score","resume","lieu","date_match",];
+            $id = $_GET['id'];
+            break;
+        default:
+            die("Mauvais lien de recherche");
+    };
+} else {
+    die("Touche pas l'url, hacker");
+}
+
+$postData = cleanPostValue($_POST);
+
+$validate = validatePost($postData, $required);
+
+if (!$validate) {
+    echo "Les champs ne sont pas remplis <br>";
+    echo buttonBack('/cdm-blog/admin/add.php?t='.$_GET['t']);
+    return;
+}
+
+
+
+
+$setData = implode(", ", array_map('transformSetdata', array_keys($postData), array_map('transformPlaceholder', array_keys($postData))));
+$setId = $id." = ". transformPlaceholder($id);
+// objectif : $setData = "$string = $holder, $string = $holder, $string = $holder etc..."
+
+var_dump($setData);
+var_dump($postData);
+
+$SQLquery =  "UPDATE $table SET $setData WHERE $setId";
+
+echo $SQLquery;
+
+/* $create = $mysqlClient->prepare($SQLquery);
+
+$create->execute($postData);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajout dans BDD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <?php include __DIR__.('/../../layout/header.php'); ?>
+
+    <div class="container">
+
+        <h1><?= $title ?> ajouté avec succès ! </h1>
+
+        <?php showPost($postData); ?>
+        <br>
+        <?=  buttonBack('/cdm-blog/public/article.php?t='.$_GET['t']); ?>
+
+    </div>
+</body>
+
+</html> */
---
+++ components/crud/update.php
@@ -0,0 +1,134 @@
+<?php
+
+require_once(__DIR__.'/../button.php');
+
+function update()
+{
+    $href = null;
+    $title = null;
+
+    if (isset($_GET['t']) && isset($_GET['id'])) {
+        switch ($_GET['t']) {
+            case "articles":
+                $href = "/cdm-blog/public/article.php?t=articles";
+                $title = "article";
+                $id = $_GET['id'];
+                return "<div class=\"container\">
+    
+                <h1 class=\"mb-3\">Modifier l'".$title." n° ".$id."</h1>
+                ".buttonBack($href)."    
+                <form action=\"../components/crud/update-post.php?t=".$_GET['t']."&id=".$_GET['id']."\" method=\"POST\">
+                
+                <!-- CHAMP : AUTEUR -->
+                <div class=\"mb-3\">
+                <label for=\"auteur\" class=\"form-label\">Auteur de l'article</label>
+                
+                <input type=\"text\" class=\"form-control\" id=\"auteur\" name=\"auteur\" value=\"\" required>
+                </div>
+
+                <!-- CHAMP : ID MATCH -->
+                
+                <input type=\"hidden\" class=\"form-control\" id=\"match_id\" name=\"match_id\" value=\"0\">
+                
+                <!-- CHAMP : DATE -->
+                
+                <input type=\"hidden\" class=\"form-control\" id=\"date_publication\" name=\"date_publication\" value=\"".date('Y-m-d')."\">
+                
+                
+                <!-- CHAMP : TITRE -->
+                <div class=\"mb-3\">
+                <label for=\"titre\" class=\"form-label\">Titre de l'article</label>
+                <input type=\"text\" class=\"form-control\" id=\"titre\" name=\"titre\" aria-describedby=\"titre-help\" required>
+                <div id=\"titre-help\" class=\"form-text\">Choisissez un titre percutant !</div>
+                </div>
+                
+                <!-- CHAMP : CONTENU -->
+                <div class=\"mb-3\">
+                <label for=\"contenu\" class=\"form-label\">contenu de l'article</label>
+                
+                <textarea class=\"form-control\" placeholder=\"Seulement du contenu vous appartenant ou libre de droits.\" id=\"contenu\" name=\"contenu\" required></textarea>
+                </div>
+                
+                <!-- BOUTONS D'ACTION -->
+                <button type=\"submit\" class=\"btn btn-primary\">Envoyer</button>
+                <br>
+                
+                </form>
+                </div>";
+
+            case "results":
+                $href = "/cdm-blog/public/resultat.php?t=results";
+                $title = "résultat";
+
+                $id = $_GET['id'];
+
+
+                return "<div class=\"container\">
+    
+                <h1 class=\"mb-3\">Modifier le ".$title." n°".$id."</h1>
+                ".buttonBack($href)."    
+                <form action=\"../components/crud/update-post.php?t=".$_GET['t']."&id=".$_GET['id']."\" method=\"POST\">
+                
+                <!-- CHAMP : equipe 1 & 2 -->
+                <div class=\"mb-3\">
+                <label for=\"equipe1\" class=\"form-label\">Equipe 1</label>
+                
+                <input type=\"text\" class=\"form-control\" id=\"equipe1\" name=\"equipe1\" required>
+                </div>
+
+                <div class=\"mb-3\">
+                <label for=\"equipe2\" class=\"form-label\">Equipe 2</label>
+                
+                <input type=\"text\" class=\"form-control\" id=\"equipe2\" name=\"equipe2\" required>
+                </div>
+
+                <!-- CHAMP : SCORE -->
+                
+                <div class=\"mb-3\">
+                <label for=\"score\" class=\"form-label\">Score</label>
+                
+                <input type=\"text\" class=\"form-control\" id=\"score\" name=\"score\" required>
+                </div>.
+
+                <!-- CHAMP : RESUME -->
+                
+                <div class=\"mb-3\">
+                <label for=\"resume\" class=\"form-label\">Resumé</label>
+                
+                <input type=\"text\" class=\"form-control\" id=\"resume\" name=\"resume\" required>
+                </div>
+
+                <!-- CHAMP : LIEU -->
+                
+                <div class=\"mb-3\">
+                <label for=\"lieu\" class=\"form-label\">Lieu du match</label>
+                
+                <input type=\"text\" class=\"form-control\" id=\"lieu\" name=\"lieu\" required>
+                </div>
+                
+                <!-- CHAMP : DATE -->
+                
+                <div class=\"mb-3\">
+                <label for=\"date_match\" class=\"form-label\">Date du match</label>
+                
+                <input type=\"date\" class=\"form-control\" id=\"date_match\" name=\"date_match\" required>
+                </div>
+                
+
+                
+                <!-- BOUTONS D'ACTION -->
+                <button type=\"submit\" class=\"btn btn-primary\">Envoyer</button>
+                <br>
+                
+                </form>
+                </div>";
+
+                break;
+            default:
+                die("Pas de tables correctement selection dans le switch");
+        };
+    } else {
+        die("Touche pas l'url, hacker");
+    }
+
+}
