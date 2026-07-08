44aaf165791a4935a255880141955cd02c80e88d
campsaurele <campsaurele@gmail.com>
2026-07-08 13:26:55 +0200

    Add: commentary

--- components/function/validatePost.php
+++ components/function/validatePost.php
@@ -1,5 +1,8 @@
 <?php
 
+// return bool to check if post required fields are not empty.
+// take required array as params so if fields is not required and can be null, it's not checked. $required is defin in switch.
+
 function validatePost(array $post, array $required): bool
 {
     foreach ($required as $field) {
